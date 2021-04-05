3b2d0aefac7aa46333a937aa06e73c6dc1aa50da
oury <amykenanmi@hotmail.com>
2021-04-05 22:29:18 +0800

    use regex to find elements

--- app/src/main/java/com/example/imgviewer/util/HttpHandler.java
+++ app/src/main/java/com/example/imgviewer/util/HttpHandler.java
@@ -9,11 +9,8 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.dom4j.Document;
-import org.dom4j.DocumentException;
-import org.dom4j.DocumentHelper;
-import org.dom4j.Node;
-import org.dom4j.io.SAXReader;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class HttpHandler {
     RequestQueue mQueue;
@@ -23,22 +20,14 @@ public class HttpHandler {
     }
 
     public void startDownload(String base_url) {
-        StringRequest stringRequest = new StringRequest(base_url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                try {
-                    ///html/body/div[4]/div/div[3]/div/div[1]/a
-                    Log.d("xpath: ", );
-                } catch (DocumentException e) {
-                    e.printStackTrace();
-                }
+        StringRequest stringRequest = new StringRequest(base_url, response -> {
+            Pattern pattern = Pattern.compile("<a class=\"movie-box\"[\\s\\S]+?href=\"([^\"]+)[\\s\\S]+?<date>([^<]+)[\\s\\S]+?</a>", Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(response);
+            if (matcher.find()) {
+                Log.d("matched", matcher.group(1));
+                Log.d("matched", matcher.group(2));
             }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("TAG", error.getMessage(), error);
-            }
-        });
+        }, error -> Log.e("TAG", error.getMessage(), error));
         mQueue.add(stringRequest);
     }
 }
